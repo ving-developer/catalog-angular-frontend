f740038657ff122fe9b5869a3a43b2a3d683b942
ving-developer <henrique.sb0709@gmail.com>
2022-05-20 23:02:37 -0300

    Adding @angular/material:material-nav menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { CategoryCreateComponent } from './category-create/category-create.compo
 import { CategoryUpdateComponent } from './category-update/category-update.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,11 +26,19 @@ import { LogoutComponent } from './logout/logout.component';
     CategoryCreateComponent,
     CategoryUpdateComponent,
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
